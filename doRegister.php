607ad16eea5b5dacd66d149b409e865f16584872
tzejian <akernus.lr@gmail.com>
2017-05-17 14:41:12 +0800

    doRegister

--- doRegister.php
+++ doRegister.php
@@ -1,4 +1,24 @@
-<?php 
+<?php
+//session_start();
+include ("dbFunctions.php");
+$msg = "";
+//retrieve details
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
 
-?>
+$query = "INSERT INTO `user`(`username`, `password`, `email`) VALUES ('$username','$password','$email')";
+
+$result = mysqli_query($link, $query) or die(mysqli_error($link));
+
+if ($result){
+    $message = "Account Created";
+    
+}
+else{
+    $message = "Try Again";
+}
+
+echo $message;
+mysqli_close($link);
