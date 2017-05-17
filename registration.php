d0ac4b56cbddfb4083bdc710e4fde93fabeecaf9
SOH YI SHENG <127589@myrp.edu.sg>
2017-05-17 11:52:12 +0800

    registration.php together with dbconns/doRegister.php

---
+++ registration.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+         <!-- Favicon -->
+        <link rel="shortcut icon" href="images/favicon/favicon1.ico">
+        <link rel="m-icon" sizes="144x144" type="image/x-icon" href="images/favicon/m-icon.png">
+
+        <!-- All CSS Plugins -->
+        <link rel="stylesheet" type="text/css" href="css/plugin.css">
+
+        <!-- Main CSS Stylesheet -->
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+
+        <!-- Google Web Fonts  -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,300,500,600,700">
+        <!-- All Javascript Plugins  -->
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/plugin.js"></script>
+        <script type="text/javascript" src="http://maps.google.com/maps/api/js?key=AIzaSyC0HAKwKinpoFKNGUwRBgkrKhF-sIqFUNA"></script>
+        
+        <!-- Main Javascript File  -->
+        <script type="text/javascript" src="js/scripts.js"></script>
+
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="UTF-8">
+        <title> Registration</title>
+    </head>
+    <body>
+        <label>Username:</label>
+        <input type="text"  name="username">
+        <label>Password:</label>
+        <input type="password" name="password">
+    </body>
+</html>
