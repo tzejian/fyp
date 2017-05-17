f3d2d0cb2057e64d93f5c30d85c8e10ebe826638
SOH YI SHENG <127589@myrp.edu.sg>
2017-05-17 21:11:07 +0800

    Added email, mobile, nric, gender fills. Improve UI design.

--- registration.php
+++ registration.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+include "dbconns.php"
+?>
 <html>
     <head>
-         <!-- Favicon -->
+        <!-- Favicon -->
         <link rel="shortcut icon" href="images/favicon/favicon1.ico">
         <link rel="m-icon" sizes="144x144" type="image/x-icon" href="images/favicon/m-icon.png">
 
@@ -25,16 +22,217 @@ and open the template in the editor.
         
         <!-- Main Javascript File  -->
         <script type="text/javascript" src="js/scripts.js"></script>
+        <script src="js/signUp.js" type="text/javascript"></script>
 
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta charset="UTF-8">
-        <title> Registration</title>
+<!--        hello im here-->
+        <title></title>
+
     </head>
     <body>
-        <label>Username:</label>
-        <input type="text"  name="username">
-        <label>Password:</label>
-        <input type="password" name="password">
+        <header id="home" class="home-section">
+
+            <div class="header-top-area" style="background-color: #237EF5">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-sm-3">
+                            <div class="logo">
+                                <a href="#home">MINDS</a>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-9">
+                            <div class="navigation-menu">
+                                <div class="navbar">
+                                    <div class="navbar-header">
+                                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                            <span class="sr-only">Toggle navigation</span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                            <span class="icon-bar"></span>
+                                        </button>
+                                    </div>
+                                    <div class="navbar-collapse collapse">
+                                        <ul class="nav navbar-nav navbar-right">
+                                            <li class="active">
+                                                <a class="smoth-scroll" href="#home">Home <div class="ripple-wrapper"></div></a>
+                                            </li>
+                                            <li><a class="smoth-scroll" href="#about">About Us</a>
+                                            </li>
+                                            <li><a class="smoth-scroll" href="#profile">Profile</a>
+                                            </li>
+                                            <li><a class="smoth-scroll" href="#donation">Donation</a>
+                                            </li>
+                                            <li><a class="smoth-scroll" href="#training">Training</a>
+                                            </li>
+                                            <li><a class="smoth-scroll" href="#adminProfile">Login</a>
+                                            </li>
+                                            <li><a class="smoth-scroll" href="#volunteer">Join US</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="home-section-background" style="background-image:url(images/bg/bg_minds.jpg) " >
+                <div class="display-table">
+                    <div class="display-table-cell">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12 text-center">
+                                    <div class="header-text">
+                                        
+                    
+                
+
+
+
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </header>
+        
+        
+        
+        <div class="col-md-8 col-md-offset-2">
+                        <div class="page-header">
+                            <p></p>
+                            <h2>Sign up</h2>
+                        </div>
+
+                        <form id="defaultForm" method="post" action="doRegister.php" class="form-horizontal">
+                            
+                              <div class="form-group">
+                                <label class="col-md-3 control-label"style="color:white">NRIC</label>
+                                <div class="col-md-5">
+                                    <input type="text" class="form-control" name="nric" required />
+                                </div>
+                            </div>
+                            
+                                                      
+                            
+                            <div class="form-group">
+                                <label class="col-md-3 control-label" style="color:white">Username</label>
+                                <div class="col-md-5">
+                                    <input type="text" class="form-control" name="username" required />
+                                </div>
+                            </div>
+                         
+
+                            <div class="form-group">
+                                <label class="col-md-3 control-label"style="color:white">Password</label>
+                                <div class="col-md-5">
+                                    <input type="password" class="form-control" name="password" required />
+                                </div>
+                            </div>
+                            
+                           
+                            
+                            <div class="form-group">
+                                <label class="col-md-3 control-label"style="color:white">Retype password</label>
+                                <div class="col-md-5">
+                                    <input type="password" class="form-control" name="confirmPassword" required/>
+                                </div>
+                            </div>
+                                   
+      
+                            
+                                  <div class="form-group">
+                                <label class="col-md-3 control-label" style="color:white">Email address</label>
+                                <div class="col-md-5">
+                                    <input type="text" class="form-control" name="email" required />
+                                </div>
+                            </div>
+                             <div class="form-group">
+                                <label class="col-md-3 control-label" style="color:white">Mobile</label>
+                                <div class="col-md-5">
+                                    <input type="text" class="form-control" name="mobile"/>
+                                </div>
+                            </div>
+                            
+                                  <div class="form-group">
+                                <label class="col-md-3 control-label" style="color:white">Gender</label>
+                                <div class="col-md-5">
+                                    <label class="radio-inline">
+                                        <input type="radio" name="gender" value='male'><p style="color: white">M</p>
+                                    </label>
+                                     <label class="radio-inline">
+                                     <input type="radio" name="gender" value='female' ><p style="color: white">F</p>
+                                    </label>
+                                   
+                                   
+                               
+                                </div>
+                            </div>
+
+                            
+
+                            <div class="form-group">
+                                <div class="col-md-9 col-md-offset-3">
+                                    <button type="submit" class="ui-btn">Sign up</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+        
+        
+        
+        
+        
+        
+
+
+       
+
+
+
+
+        <!-- Footer Start -->
+        <footer class="footer-section">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-6 text-left">
+                        <p><span><a href="#about" class="smoth-scroll">About Us</a></span> | <span><a href="#" class="smoth-scroll">MINDS</a></span></p>
+                    </div>
+
+                    <div class="col-md-6 text-right">
+                        <ul class="social-icon">
+                            <li><a href="#" target="_blank" class="facebook"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#" target="_blank" class="twitter"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#" target="_blank" class="google-plus"><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href="#" target="_blank" class="instagram"><i class="fa fa-instagram"></i></a></li>
+
+                        </ul>
+                    </div>
+
+
+                </div>
+            </div>
+        </footer>
+        <!-- Footer End -->
+
+
+        <!-- Back to Top Start -->
+        <a href="#" class="scroll-to-top"><i class="fa fa-angle-up"></i></a>
+        <!-- Back to Top End -->
+
+
+
+
+
+
     </body>
 </html>
