768b28ee362a0e24b8b6854c41002db19ce9a456
127589 <127589@myrp.edu.sg>
2017-05-24 21:35:04 +0800

    doLogin.php

---
+++ doLogin.php
@@ -0,0 +1,25 @@
+<?php
+
+include "dbconns.php";
+
+$entered_username = $_POST['username'];
+$entered_password = $_POST['password'];
+
+
+$msg = "";
+
+$queryCheck = "SELECT * FROM user WHERE username = '$entered_username' AND password = SHA1('$entered_password')";
+$resultCheck = mysqli_query($link, $queryCheck) or die(mysqli_error($link));
+
+// 1 matching row with username and password is found in table: user is valid
+if(mysqli_num_rows($resultCheck) == 1) {
+    $row = mysqli_fetch_array($resultCheck);
+    $_SESSION['user_id'] = $row['id'];
+    $_SESSION['username'] = $row['username'];
+    
+    $msg = "You are logged in as ".$_SESSION['username']."";
+    $msg = "<p><a href='index.php'>Home</a></p>";
+} else {
+    $msg = "<p>Sorry, you must enter a valid username and password to log in.";
+}
+?>
