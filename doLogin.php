dcca9fb0bd00e6fcdb93cd7f3aae042f7f4a8854
tzejian <akernus.lr@gmail.com>
2017-05-25 13:15:39 +0800

    doLogin.php

--- doLogin.php
+++ doLogin.php
@@ -1,26 +1,63 @@
 <?php
-
-include "dbconns.php";
-
-$entered_username = $_POST['username'];
-$entered_password = $_POST['password'];
-
+// php file that contains the common database connection code
+session_start();
+include "dbFunctions.php";
+$enteredUsername = $_POST['username'];
+$enteredPassword = $_POST['password'];
 
 $msg = "";
 
-$queryCheck = "SELECT * FROM user WHERE username = '$entered_username' AND password = SHA1('$entered_password')";
-$resultCheck = mysqli_query($link, $queryCheck) or die(mysqli_error($link));
+$queryCheck = "Select * from user where username='$enteredUsername' and password = ('$enteredPassword')";
 
-// 1 matching row with username and password is found in table: user is valid
-if(mysqli_num_rows($resultCheck) == 1) {
-    session_start();
+$resultCheck = mysqli_query($link, $queryCheck)or die(mysqli_error($link));
+
+if(mysqli_num_rows($resultCheck) == 1){ //check for matching records
     $row = mysqli_fetch_array($resultCheck);
-    $_SESSION['user_id'] = $row['id'];
-    $_SESSION['username'] = $row['username'];
+    $_SESSION['id'] = $row['id'];
+    $_SESSION['username'] = $row['username']; //assign user id from $row to session
+    $_SESSION['name'] = $row['name'];
     
-    $msg = "You are logged in as ".$_SESSION['username']."";
-    $msg = "<p><a href='index.php'>Home</a></p>";
-} else {
-    $msg = "<p>Sorry, you must enter a valid username and password to log in.";
+    $msg = "<h2>You are logged in as " .$_SESSION['username']."</h2>";
+    $msg .= "<p><a href='index.php'>Home</a>&nbsp<a href='logout.php'>get off</a></p>.</br>";
+    
+
 }
+else{
+    $msg = "<p>Sorry, get out</p>";
+    $msg .= "<p><a href='login.php'>Go back to where u belong</a></p>";
+
+}
+
+
+
+
+mysqli_close($link);
+
+
+
+/* Redirect browser */
+header("Location: index.php");
+
+ 
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+
+
+
 ?>
+<script type="text/javascript">
+     alert($msg); 
+</script>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title></title>
+    </head>
+    <body style="background-color:lightcyan;">
+        <?php
+        echo $msg;
+        ?>
+    </body>
+</html>
