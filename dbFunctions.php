6901368f2dfc267c68020fd99725b08e91067052
127589 <127589@myrp.edu.sg>
2017-05-28 13:01:50 +0800

    dbfunctions.php

--- dbFunctions.php
+++ dbFunctions.php
@@ -2,7 +2,7 @@
 $db_host = "localhost";
 $db_username = "root";
 $db_password = "";
-$db_name = "website_login";
+$db_name = "fyp";
 $link = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 
 if (!$link) {
