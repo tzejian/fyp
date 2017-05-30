15c91c7c93fa5cfcfd0ae0c236e645fc0f00f9eb
tzejian <akernus.lr@gmail.com>
2017-05-31 00:13:26 +0800

    added a quiz examples inside

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ session_start();
 
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-       
+
         <title></title>
 
     </head>
@@ -36,49 +36,41 @@ session_start();
             <?php include("navbarlogin.php") ?>
 
             <div class="home-section-background" style="background-image:url(images/bg/Minds_4.jpg)" >
-                
-                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                        <!-- Indicators -->
-                        <ol class="carousel-indicators">
-                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                            <li data-target="#myCarousel" data-slide-to="1"></li>
-                            <li data-target="#myCarousel" data-slide-to="2"></li>
-                        </ol>
 
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner">
-                            <div class="item active">
-                                <img src="images/bg/mind_2.jpg" style="width: 100%;height: 100%">
-                            </div>
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
 
-                            <div class="item">
-                                <img src="images/bg/minds_3.jpg"  style="width: 100%;height: 100%">
-                            </div>
-
-                            <div class="item">
-                                <img src="images/bg/bg_minds.jpg"  style="width: 100%;height: 100%">
-                            </div>
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="images/bg/mind_2.jpg" style="width: 100%;height: 100%">
                         </div>
 
-                        <!-- Left and right controls -->
-                        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                            <span class="glyphicon glyphicon-chevron-left"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                            <span class="glyphicon glyphicon-chevron-right"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
+                        <div class="item">
+                            <img src="images/bg/minds_3.jpg"  style="width: 100%;height: 100%">
+                        </div>
+
+                        <div class="item">
+                            <img src="images/bg/bg_minds.jpg"  style="width: 100%;height: 100%">
+                        </div>
                     </div>
-                
 
-
-
-
-
-
+                    <!-- Left and right controls -->
+                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
             </div>
-
         </header>
 
 
@@ -194,11 +186,31 @@ session_start();
 
 
 
-        <section id="training" class="testimonial-section section-space-padding">
+        <section id="training" class="section-space-padding">
             <div class="container">
                 <div class="row">
                     <div class="col-sm-12">
                         <div class="section-title">
+                            <h2>Quiz examples</h2>
+
+                            <script>
+                                (function (d, s, id) {
+                                    var js,
+                                            fjs = d.getElementsByTagName(s)[0],
+                                            p = (('https:' == d.location.protocol) ? 'https://' : 'http://'),
+                                            r = Math.floor(new Date().getTime() / 1000000);
+                                    if (d.getElementById(id)) {
+                                        return;
+                                    }
+                                    js = d.createElement(s);
+                                    js.id = id;
+                                    js.async = 1;
+                                    js.src = p + "www.opinionstage.com/assets/loader.js?" + r;
+                                    fjs.parentNode.insertBefore(js, fjs);
+                                }(document, 'script', 'os-widget-jssdk'));
+                            </script>
+                            <div id="34086" class="os_widget" data-path="/leetzejian/testing" data-of="leetzejian" data-comments="false" data-width="450" ></div>
+
 
                                 
                         </div>
