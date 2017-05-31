2146e928981db769752fde67966492047a2a19a6
127589 <127589@myrp.edu.sg>
2017-05-31 11:16:08 +0800

    doForgotPassword.php

--- doForgotPassword.php
+++ doForgotPassword.php
@@ -1,18 +1,27 @@
 <?php
-include "dbconns.php";
+include "dbFunctions.php";
 
 $newPassword = createRandomPassword();
 $enteredEmail = $_POST['email'];
+
+
 
 $updateQuery = "UPDATE user SET password = SHA1('$newPassword') WHERE email = '$enteredEmail'";
 $updated = mysqli_query($link, $updateQuery) or die(mysqli_error($link));
 
 if ($updated) {
-    $subject = "";
+   
+    $subject = "MINDS Volunteer App: Password RESET";
     $message = "Your new password is $newPassword";
     $headers = "From: minds@org.edu.sg";
-    $emailSent = mail($email, $subject, $message, $headers);
+    $emailSent = mail($enteredEmail, $subject, $message, $headers);
     if ($emailSent) {
         $statusMsg = "The email has been sent!";
     }
+}
+
+function createRandomPassword () {
+    $min = 7491;
+    $max = 184521;
+    mt_rand($min, $max);
 }
