b4e772700281b0ac3b516007b9d9c5b01cb2c190
127589 <127589@myrp.edu.sg>
2017-05-31 11:25:44 +0800

    doRegister.php

--- doRegister.php
+++ doRegister.php
@@ -6,9 +6,15 @@ $msg = "";
 $username = $_POST['username'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+$nric = $_POST['nric'];
+$gender = $_POST['gender'];
+$mobile = $_POST['mobile'];
 
 
-$query = "INSERT INTO `user`(`username`, `password`, `email`) VALUES ('$username','$password','$email')";
+
+
+
+$query = "INSERT INTO `user`(`username`, `password`, `email`, `nric`, `mobile`, `gender`) VALUES ('$username', SHA1('$password'),'$email', '$nric', '$mobile', '$gender')";
 
 $result = mysqli_query($link, $query) or die(mysqli_error($link));
 
