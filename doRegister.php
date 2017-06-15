f96fb1195c2df340e456d5506183ecf82ca6b8ef
15017117 <15017117@myrp.edu.sg>
2017-06-15 22:55:36 +0800

    viewVideo(php)
    store into blacklist

--- doRegister.php
+++ doRegister.php
@@ -27,8 +27,11 @@ if (mysqli_num_rows($checkResult) == 0) {
 } else {
 //    $message1 = "NRIC Already Exists.\\nTry again.";
 //  echo "<script type='text/javascript'>alert('$message1')</script>";
-    
-    header("location: registration.php");
+    $queryblacklist = "INSERT INTO `blacklist`(`username`, `nric`) VALUES ('$username','$nric')";
+    $resultblacklist = mysqli_query($link, $queryblacklist) or die(mysqli_error($link));
+    if($resultblacklist){
+         header("location: registration.php");
+    }
     
 }
 echo $message;
