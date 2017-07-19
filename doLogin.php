9a60d683f9264338f93ede6f40fc72f2b1090009
127589 <127589@myrp.edu.sg>
2017-07-19 12:06:08 +0800

    doLOgin

--- doLogin.php
+++ doLogin.php
@@ -9,7 +9,7 @@ if (!isset($SESSION['id'])) {
         $enteredUsername = $_POST['username'];
         $enteredPassword = $_POST['password'];
         
-        $queryCheck = "SELECT id,username,email FROM user WHERE username='" . $enteredUsername . "' AND password = SHA1('" . $enteredPassword . "')";
+        $queryCheck = "SELECT * FROM user WHERE username='" . $enteredUsername . "' AND password = SHA1('" . $enteredPassword . "')";
         
         
         $resultCheck = mysqli_query($link, $queryCheck)or die(mysqli_error($link));
@@ -17,8 +17,13 @@ if (!isset($SESSION['id'])) {
         if (mysqli_num_rows($resultCheck)  > 0) { //check for matching records
             $row = mysqli_fetch_array($resultCheck);
             $_SESSION['id'] = $row['id'];
+            $_SESSION['password'] = $row['password'];
             $_SESSION['username'] = $row['username']; //assign user id from $row to session
             $_SESSION['email'] = $row['email'];
+            $_SESSION['fullname'] = $row['fullname'];
+            $_SESSION['mobile'] = $row['mobile'];
+            $_SESSION['gender'] = $row['gender'];
+            $_SESSION['role'] =  $row['role'];
 
             /* Redirect browser */
             header("Location: index.php");
